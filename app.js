01a31d90ecf37b4cf6eb070e94545191ee31a00c
Md. Mubdiur Rahman <mubdiur@gmail.com>
2022-01-30 20:35:06 +0600

    updated app

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+    res.end("Online!");
 });
 server.listen();
